213deb7367245fd1e9ed0ebb9917afd0765b1d75
Dogus Kucukgode <doguskucukgode@gmail.com>
2020-09-20 19:41:24 +0300

    updated read me

--- backend/src/main/kotlin/com/d2d/allygator/shuttle/model/Vehicle.kt
+++ backend/src/main/kotlin/com/d2d/allygator/shuttle/model/Vehicle.kt
@@ -1,18 +1,14 @@
 package com.d2d.allygator.shuttle.model
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import lombok.ToString
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@ToString
 @Document(collection = "vehicle")
 class Vehicle {
 
     @Id
     var id: String? = null
     var lastLocation: Location? = null
-    @JsonIgnore
     var deleted: Boolean = false
 }
 
